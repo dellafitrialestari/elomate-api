d513aee42b19bcdb45b7141d05073389b151df53
Della fitria lestari <dellafitrialestari777@gmail.com>
2024-11-06 16:30:15 +0700

    API user

--- src/models/users.js
+++ src/models/users.js
@@ -1,18 +1,18 @@
 const dbPool = require('../config/database');
 
 const getAllUsers = () => {
-    const SQLQuery = 'SELECT * FROM user_';
+    const SQLQuery = 'SELECT * FROM user';
     return dbPool.execute(SQLQuery);
 }
 
 const getUserById = (idUser) => {
-    const SQLQuery = `SELECT * FROM user_ WHERE user_id=${idUser}`;
+    const SQLQuery = `SELECT * FROM user WHERE user_id=${idUser}`;
     return dbPool.execute(SQLQuery);
 }
 
 const createNewUser = (body) => {
-    const SQLQuery = `INSERT INTO user_ (assessment_id, batch_data_batch_id, role_id, nama_lengkap, nrp, password_, email, posisi, asal_universitas, jurusan, tempat_lahir, tanggal_lahir, domisili, no_hp) 
-                      VALUES ('${body.assessment_id}', '${body.batch_data_batch_id}', '${body.role_id}', '${body.nama_lengkap}', '${body.nrp}', 
+    const SQLQuery = `INSERT INTO user (batch_data_batch_id, role_id, nama_lengkap, nrp, password_, email, posisi, asal_universitas, jurusan, tempat_lahir, tanggal_lahir, domisili, no_hp) 
+                      VALUES ('${body.batch_data_batch_id}', '${body.role_id}', '${body.nama_lengkap}', '${body.nrp}', 
                       '${body.password_}', '${body.email}', '${body.posisi}', '${body.asal_universitas}', '${body.jurusan}', 
                       '${body.tempat_lahir}', '${body.tanggal_lahir}', '${body.domisili}', '${body.no_hp}')`;
 
@@ -20,7 +20,7 @@ const createNewUser = (body) => {
 }
 
 const updateUser = (body, idUser) => {
-    const SQLQuery = `UPDATE user_ 
+    const SQLQuery = `UPDATE user 
                       SET nama_lengkap = ?, email = ?, domisili = ? 
                       WHERE user_id = ?`;
     const values = [body.nama_lengkap, body.email, body.domisili, idUser];
@@ -29,7 +29,7 @@ const updateUser = (body, idUser) => {
 }
 
 const deleteUser = (idUser) => {
-    const SQLQuery = `DELETE FROM user_ WHERE user_id = ?`;
+    const SQLQuery = `DELETE FROM user WHERE user_id = ?`;
     return dbPool.execute(SQLQuery, [idUser]);
 }
 
